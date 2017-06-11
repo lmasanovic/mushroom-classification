ee5e333bac82ea95cc8ba31cb0054296228f4e30
Luka Masanovic <lukamasanovic@gmail.com>
2017-06-11 14:34:59 +0200

    add not sure option

--- script.js
+++ script.js
@@ -24,8 +24,26 @@ function createForm(json) {
 			div.append(label);
 			divs.push(div);
 		});
+		var div = $(document.createElement("div")).addClass("form-check");
+		var label = ($(document.createElement("label"))
+		.addClass("form-check-label"))
+		.html(" Not sure");
+
+		label.prepend($(document.createElement("input")).addClass("form-check-input")
+		.attr({
+			type: "radio",
+			name: "not sure",
+			value: "null"
+		}));
+		div.append(label);
+		divs.push(div);
 
 		fieldset.append(divs);
 		$("form").prepend(fieldset);
 	});
+}
+
+
+function createDivForForms(name, value) {
+
 }
