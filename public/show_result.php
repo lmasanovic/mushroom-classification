aff801eb7188e51d782b7dcdefa4ec15588cc6e9
fkulic <filip.kulic@outlook.com>
2017-06-12 17:29:12 +0200

    add images on show result

--- public/show_result.php
+++ public/show_result.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
-echo $_SESSION["class"] . " " . $_SESSION["probability"];
-session_unset();
+if (isset($_SESSION["class"]) && isset($_SESSION["probability"])) {
+	$edible = $_SESSION["class"] == "e" ? "edible" : "poisonus";
+	$probability = $_SESSION["probability"];
+} else {
+	header("Location: index.php");
+}
+
+echo "<h1>Mushroom is " . $edible . ".</h1>";
+echo "<img src=images/" . $edible . "_mushroom.jpg />"
+
 ?>
--- scripts/get_form_data.php
+++ scripts/get_form_data.php
@@ -1,9 +1,16 @@
 <?php
+session_start();
+session_unset();
 $properties;
 $values;
 if(isset($_POST["submit"])){
 	$properties = $_POST;
 	unset($properties["submit"]);
+	foreach ($properties as $property => $value) {
+		if($value != "null") {
+			$_SESSION[$property] = $value;
+		}
+	}
 	$properties = array("class" => "null") + $properties;
 	$values = array_values($properties);
 	$properties = array_keys($properties);
