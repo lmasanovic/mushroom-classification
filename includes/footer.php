6cbbfc8b6b2ed97fa6ec772b146e74c75fd94f3d
Luka Masanovic <lukamasanovic@gmail.com>
2017-06-12 17:35:49 +0200

    add includes folder

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Mashroom Classficitaor</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-    <div class="container">
-        <form action="../scripts/get_form_data.php" method="post"></form>
-    </div>
-</body>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="js/script.js"></script>
+<?php include("../includes/header.php") ?>
 
-</html>
+<div class="container">
+    <form action="../scripts/get_form_data.php" method="post"></form>
+</div>
+
+<?php include("../includes/footer.php") ?>
