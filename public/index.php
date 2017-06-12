ba5a5767d48429f24b4d30a59bc0c6417ff9e6c4
Luka Masanovic <lukamasanovic@gmail.com>
2017-06-12 18:58:44 +0200

    minor fix

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php include("../includes/header.php") ?>
 
 <div class="container">
-    <form action="../scripts/get_form_data.php" method="post"></form>
+    <form action="./../scripts/get_form_data.php" method="post"></form>
 </div>
 
 <?php include("../includes/footer.php") ?>
