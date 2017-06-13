b17cfef040c78dab5f5545df9d1aeac516c6134f
Luka Masanovic <lukamasanovic@gmail.com>
2017-06-13 13:51:46 +0200

    add style to show_result

--- show_result.php
+++ show_result.php
@@ -1,12 +1,30 @@
+<?php include("includes/header.php"); ?>
 <?php
-session_start();
+$probability;
+$edible;
 if (isset($_SESSION["class"]) && isset($_SESSION["probability"])) {
-	$edible = $_SESSION["class"] == "e" ? "edible" : "poisonus";
 	$probability = $_SESSION["probability"];
+	if($_SESSION["class"] == "e") {
+		$probability = 1 - $probability;
+		$edible = "edible";
+	} else {
+		$edible = "poisonus";
+	}
+	$probability *= 100;
+
 } else {
 	header("Location: index.php");
 }
-
-echo "<h1>Mushroom is " . $edible . ".</h1>";
-echo "<img src=images/" . $edible . "_mushroom.jpg />";
 ?>
+<div class="container">
+
+	<h1>I'm <?php echo $probability; ?>% sure this mushroom is <?php echo $edible; ?>! </h1>
+	<img src="images/<?php echo $edible; ?>_mushroom.jpg" class="img-circle">
+
+	<div>
+		<a href="index.php" class="btn btn-primary btn-large "><i class="glyphicon glyphicon-chevron-left"></i> Back</a>
+	</div>
+</div>
+
+
+<?php include("includes/footer.php"); ?>
